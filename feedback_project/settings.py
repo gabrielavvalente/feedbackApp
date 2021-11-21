34492e5d21ebd04982b37b5c3f94720e41733c4e
Gabriela Valente <gabriela.vvalente@gmail.com>
2021-11-20 23:40:54 -0300

    security django config

--- feedback_project/settings.py
+++ feedback_project/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z3o1hwtvrapf(d8d&l1dr559p^g=r7qdd05zadt9x2x!cayjof'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
