1f337288055584e3971ab6a3a8c8e4fac6bdfdcd
Gabriela Valente <gabriela.vvalente@gmail.com>
2021-11-22 19:33:17 -0300

    adding template html and site css def

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
-#teste
 from django.urls import path
 from main import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("main/<name>", views.hello_there, name="hello_there"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
