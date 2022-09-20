0688e10f3b1d8714dcd47888c5e4198c6cd5016f
Gabriela Valente <gabriela.vvalente@gmail.com>
2022-09-19 21:52:37 -0300

    linking admin page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from main import views
+from main import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.home, name="home"),
+    #path("/admin", admin.site.urls),
     path("main/<name>", views.hello_there, name="hello_there"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact")
